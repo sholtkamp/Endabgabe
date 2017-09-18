42762f6751f2dea399e89750fb859a231a1875f6
sholtkamp <S.Kokon93@gmail.com>
2017-09-18 14:45:20 +0200

    npm start working

--- app.js
+++ app.js
@@ -10,20 +10,18 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'app_client/img', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+
+app.use('/', express.static(__dirname + "/app_client"));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/app_client/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,3 +42,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
