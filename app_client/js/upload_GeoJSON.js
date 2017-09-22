f5edc84a6ba4360ebd3a6eea659b2512759485b1
sholtkamp <S.Kokon93@gmail.com>
2017-09-22 10:18:27 +0200

    init upload json control

--- app_client/js/upload_GeoJSON.js
+++ app_client/js/upload_GeoJSON.js
@@ -7,12 +7,40 @@ document.getElementById("GeoJSON_input").addEventListener("change", function (ev
         reader = new FileReader();
 
     reader.onload = function (e) {
-        var fileText = e.target.result,
-            fileData = JSON.parse(fileText),
-            group = L.geoJSON(fileData).addTo(map);
+        logger.info("Upload started");
+        var fileText = e.target.result;
+        // var extension = getExtension(fileText);
+        // logger.info("Files extension: " + extension);
+        // var right_type = isJSON(extension);
+        //
+        // if (right_type == true) {
 
-        map.fitBounds(group.getBounds());
+            logger.info("File is a .JSON file");
+            fileData = JSON.parse(fileText);
+            group = L.geoJSON(fileData).addTo(map);
+            map.fitBounds(group.getBounds());
+        // }
+        //
+        // else {
+        //     logger.info("File is NOT a .JSON file")
+        // }
     };
 
     reader.readAsText(file);
 });
+
+// function getExtension(filename) {
+//     var parts = filename.split('.');
+//     return parts[parts.length - 1];
+// }
+//
+// function isJSON(filename) {
+//     var ext = getExtension(filename);
+//     switch (ext.toLowerCase()) {
+//         case 'application/json':
+//             return true;
+//     }
+//     return false;
+// }
+
+// https://stackoverflow.com/questions/7977084/check-file-type-when-form-submit
